8508db6baed805c8c7af68d90f95786d70481e8b
Peter Rushforth <peter.rushforth@canada.ca>
2018-10-05 12:22:46 -0400

    Update grunt and its dependencies due to vulnerability warnings.

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-html');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
