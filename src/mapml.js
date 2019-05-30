260ae25d7cb12f4d33472a783085da63cbbae434
prushforth <peter.rushforth@canada.ca>
2019-05-30 13:52:41 -0400

    Refactor query support from templated layer (mostly, hopefully as appropriate) into query custom handler at the map level.

--- src/mapml.js
+++ src/mapml.js
@@ -39,6 +39,7 @@
  * Title to copyright in this work will at all times remain with copyright holders.
  */
 /* global L, Node */
+/*jshint esversion: 6 */
 (function (window, document, undefined) {
   
 var M = {};
@@ -330,7 +331,6 @@ M.QueryHandler = L.Handler.extend({
         L.DomEvent.on(this._map, 'click', this._queryTopLayer, this);
         L.DomEvent.on(this._map, 'keypress', this._queryTopLayerAtMapCenter, this);
     },
-
     removeHooks: function() {
         L.DomEvent.off(this._map, 'click', this._queryTopLayer, this);
         L.DomEvent.on(this._map, 'keypress', this._queryTopLayerAtMapCenter, this);
@@ -353,13 +353,129 @@ M.QueryHandler = L.Handler.extend({
               containerPoint: this._map.latLngToContainerPoint(this._map.getCenter())
           });
         }
-      
     },
     _queryTopLayer: function(event) {
         var layer = this._getTopQueryableLayer();
         if (layer) {
-            layer._templatedLayer.handleClick(event);
+            this._query(event, layer);
         }
+    },
+    _query(e, layer) {
+      var obj = {},
+          template = layer.getQueryTemplates()[0],
+          bounds = e.target.getPixelBounds(),
+          zoom = e.target.getZoom(),
+          map = this._map,
+          crs = map.options.crs,
+          container = layer._container,
+          popupOptions = {autoPan: true, maxHeight: (map.getSize().y * 0.5) - 50},
+          tcrs2pcrs = function (c) {
+            return crs.transformation.untransform(c,crs.scale(zoom));
+          };
+          
+      var tcrsClickLoc = map.getPixelOrigin().add(e.layerPoint),
+          tileMatrixClickLoc = tcrsClickLoc.divideBy(256).floor(),
+          tileBounds = new L.Bounds(tcrsClickLoc.divideBy(256).floor().multiplyBy(256), tcrsClickLoc.divideBy(256).ceil().multiplyBy(256));
+  
+      // all of the following are locations that might be used in a query, I think.
+      obj[template.query.tilei] = tcrsClickLoc.x.toFixed() - (tileMatrixClickLoc.x * 256);
+      obj[template.query.tilej] = tcrsClickLoc.y.toFixed() - (tileMatrixClickLoc.y * 256);
+      obj[template.query.mapi] = e.containerPoint.x.toFixed();
+      obj[template.query.mapj] = e.containerPoint.y.toFixed();
+      
+      obj[template.query.col] = tileMatrixClickLoc.x;
+      obj[template.query.row] = tileMatrixClickLoc.y;
+
+      // whereas the layerPoint is calculated relative to the origin plus / minus any
+      // pan movements so is equal to containerPoint at first before any pans, but
+      // changes as the map pans. 
+      obj[template.query.x] = tcrsClickLoc.x.toFixed();
+      obj[template.query.y] = tcrsClickLoc.y.toFixed();
+      obj[template.query.easting] =  tcrs2pcrs(map.getPixelOrigin().add(e.layerPoint)).x;
+      obj[template.query.northing] = tcrs2pcrs(map.getPixelOrigin().add(e.layerPoint)).y;
+      obj[template.query.zoom] = zoom;
+      obj[template.query.width] = map.getSize().x;
+      obj[template.query.height] = map.getSize().y;
+      obj[template.query.mapbottom] = tcrs2pcrs(bounds.max).y;
+      obj[template.query.mapleft] = tcrs2pcrs(bounds.min).x;
+      obj[template.query.maptop] = tcrs2pcrs(bounds.min).y;
+      obj[template.query.mapright] = tcrs2pcrs(bounds.max).x;
+      
+      obj[template.query.tilebottom] = tcrs2pcrs(tileBounds.max).y;
+      obj[template.query.tileleft] = tcrs2pcrs(tileBounds.min).x;
+      obj[template.query.tiletop] = tcrs2pcrs(tileBounds.min).y;
+      obj[template.query.tileright] = tcrs2pcrs(tileBounds.max).x;
+      // add hidden or other variables that may be present into the values to
+      // be processed by L.Util.template below.
+      for (var v in template.query) {
+          if (["mapi","mapj","tilei","tilej","row","col","x","y","easting","northing","width","height","zoom","mapleft","mapright",",maptop","mapbottom","tileleft","tileright","tiletop","tilebottom"].indexOf(v) < 0) {
+              obj[v] = template.query[v];
+          }
+      }
+      fetch(L.Util.template(template.template, obj),{redirect: 'follow'}).then(
+          function(response) {
+            if (response.status >= 200 && response.status < 300) {
+              return Promise.resolve(response);
+            } else {
+              console.log('Looks like there was a problem. Status Code: ' + response.status);
+              return Promise.reject(response);
+            }
+          }).then(function(response) {
+            var contenttype = response.headers.get("Content-Type");
+            if ( contenttype === "text/mapml") {
+              return handleMapMLResponse(response, e.latlng);
+            } else {
+              return handleOtherResponse(response, layer, e.latlng);
+            }
+          }).catch(function(err) {
+            // no op
+          });
+      function handleMapMLResponse(response, loc) {
+          return response.text().then(mapml => {
+              var _unproject = L.bind(map.options.crs.unproject, map.options.crs);
+              function _coordsToLatLng(coords) {
+                  return _unproject(L.point(coords));
+              }
+              var parser = new DOMParser(),
+                  mapmldoc = parser.parseFromString(mapml, "application/xml"),
+                  f = M.mapMlFeatures(mapmldoc, {
+                  // pass the vector layer a renderer of its own, otherwise leaflet
+                  // puts everything into the overlayPane
+                  renderer: L.svg(),
+                  // pass the vector layer the container for the parent into which
+                  // it will append its own container for rendering into
+                  pane: container,
+                  color: 'yellow',
+                  // instead of unprojecting and then projecting and scaling,
+                  // a much smarter approach would be to scale at the current
+                  // zoom
+                  coordsToLatLng: _coordsToLatLng,
+                  imagePath: M.detectImagePath(map.getContainer())
+              });
+              f.addTo(map);
+
+              var c = document.createElement('iframe');
+              c.csp = "script-src 'none'";
+              c.style = "border: none";
+              c.srcdoc = mapmldoc.querySelector('feature properties').innerHTML;
+
+              // passing a latlng to the popup is necessary for when there is no
+              // geometry / null geometry
+              layer.bindPopup(c, popupOptions).openPopup(loc);
+              layer.on('popupclose', function() {
+                  map.removeLayer(f);
+              });
+          });
+      }
+      function handleOtherResponse(response, layer, loc) {
+          return response.text().then(text => {
+              var c = document.createElement('iframe');
+              c.csp = "script-src 'none'";
+              c.style = "border: none";
+              c.srcdoc = text;
+              layer.bindPopup(c, popupOptions).openPopup(loc);
+          });
+      }
     }
 });
 L.Map.addInitHook('addHandler', 'query', M.QueryHandler);
@@ -1486,6 +1602,11 @@ M.MapMLLayer = L.Layer.extend({
         var projection = this.getProjection();
         if (!map.options.projection || projection !== 'WGS84' && map.options.projection !== projection) return false;
         return true;
+    },
+    getQueryTemplates: function() {
+        if (this._templatedLayer && this._templatedLayer._queries) {
+          return this._templatedLayer._queries;
+        }
     }
 });
 M.mapMLLayer = function (url, node, options) {
@@ -2166,131 +2287,13 @@ M.TemplatedLayer = L.Layer.extend({
       }
     }
   },
-  handleClick(e) {
-      var obj = {},
-          layer = this,
-          template = this._queries[0],
-          bounds = e.target.getPixelBounds(),
-          zoom = e.target.getZoom(),
-          map = e.target,
-          crs = map.options.crs,
-          container = layer._container,
-          popupOptions = {autoPan: true, maxHeight: (layer._map.getSize().y * 0.5) - 50},
-          tcrs2pcrs = function (c) {
-            return crs.transformation.untransform(c,crs.scale(zoom));
-          };
-          
-      var tcrsClickLoc = map.getPixelOrigin().add(e.layerPoint),
-          tileMatrixClickLoc = tcrsClickLoc.divideBy(256).floor(),
-          tileBounds = new L.Bounds(tcrsClickLoc.divideBy(256).floor().multiplyBy(256), tcrsClickLoc.divideBy(256).ceil().multiplyBy(256));
-  
-      // all of the following are locations that might be used in a query, I think.
-      obj[template.query.tilei] = tcrsClickLoc.x.toFixed() - (tileMatrixClickLoc.x * 256);
-      obj[template.query.tilej] = tcrsClickLoc.y.toFixed() - (tileMatrixClickLoc.y * 256);
-      obj[template.query.mapi] = e.containerPoint.x.toFixed();
-      obj[template.query.mapj] = e.containerPoint.y.toFixed();
-      
-      obj[template.query.col] = tileMatrixClickLoc.x;
-      obj[template.query.row] = tileMatrixClickLoc.y;
-
-      // whereas the layerPoint is calculated relative to the origin plus / minus any
-      // pan movements so is equal to containerPoint at first before any pans, but
-      // changes as the map pans. 
-      obj[template.query.x] = tcrsClickLoc.x.toFixed();
-      obj[template.query.y] = tcrsClickLoc.y.toFixed();
-      obj[template.query.easting] =  tcrs2pcrs(map.getPixelOrigin().add(e.layerPoint)).x;
-      obj[template.query.northing] = tcrs2pcrs(map.getPixelOrigin().add(e.layerPoint)).y;
-      obj[template.query.zoom] = zoom;
-      obj[template.query.width] = map.getSize().x;
-      obj[template.query.height] = map.getSize().y;
-      obj[template.query.mapbottom] = tcrs2pcrs(bounds.max).y;
-      obj[template.query.mapleft] = tcrs2pcrs(bounds.min).x;
-      obj[template.query.maptop] = tcrs2pcrs(bounds.min).y;
-      obj[template.query.mapright] = tcrs2pcrs(bounds.max).x;
-      
-      obj[template.query.tilebottom] = tcrs2pcrs(tileBounds.max).y;
-      obj[template.query.tileleft] = tcrs2pcrs(tileBounds.min).x;
-      obj[template.query.tiletop] = tcrs2pcrs(tileBounds.min).y;
-      obj[template.query.tileright] = tcrs2pcrs(tileBounds.max).x;
-      // add hidden or other variables that may be present into the values to
-      // be processed by L.Util.template below.
-      for (var v in template.query) {
-          if (["mapi","mapj","tilei","tilej","row","col","x","y","easting","northing","width","height","zoom","mapleft","mapright",",maptop","mapbottom","tileleft","tileright","tiletop","tilebottom"].indexOf(v) < 0) {
-              obj[v] = template.query[v];
-          }
+  onAdd: function (map) {
+    for (var i=0;i<this._templates.length;i++) {
+      if (this._templates[i].type !== 'query') {
+        map.addLayer(this._templates[i].layer);
       }
-      fetch(L.Util.template(template.template, obj),{redirect: 'follow'}).then(
-          function(response) {
-            if (response.status >= 200 && response.status < 300) {
-              return Promise.resolve(response);
-            } else {
-              console.log('Looks like there was a problem. Status Code: ' + response.status);
-              return Promise.reject(response);
-            }
-          }).then(function(response) {
-            var contenttype = response.headers.get("Content-Type");
-            if ( contenttype === "text/mapml") {
-              return handleMapMLResponse(response, e.latlng);
-            } else {
-              return handleOtherResponse(response, layer, e.latlng);
-            }
-          }).catch(function(err) {
-            // no op
-          });
-        function handleMapMLResponse(response, loc) {
-            return response.text().then(mapml => {
-                var _unproject = L.bind(map.options.crs.unproject, map.options.crs);
-                function _coordsToLatLng(coords) {
-                    return _unproject(L.point(coords));
-                }
-                var parser = new DOMParser(),
-                    mapmldoc = parser.parseFromString(mapml, "application/xml"),
-                    f = M.mapMlFeatures(mapmldoc, {
-                    // pass the vector layer a renderer of its own, otherwise leaflet
-                    // puts everything into the overlayPane
-                    renderer: L.svg(),
-                    // pass the vector layer the container for the parent into which
-                    // it will append its own container for rendering into
-                    pane: container,
-                    color: 'yellow',
-                    // instead of unprojecting and then projecting and scaling,
-                    // a much smarter approach would be to scale at the current
-                    // zoom
-                    coordsToLatLng: _coordsToLatLng,
-                    imagePath: M.detectImagePath(map.getContainer())
-                });
-                f.addTo(map);
-
-                var c = document.createElement('iframe');
-                c.csp = "script-src 'none'";
-                c.style = "border: none";
-                c.srcdoc = mapmldoc.querySelector('feature properties').innerHTML;
-                
-                // passing a latlng to the popup is necessary for when there is no
-                // geometry / null geometry
-                layer.bindPopup(c, popupOptions).openPopup(loc);
-                layer.on('popupclose', function() {
-                    map.removeLayer(f);
-                });
-            });
-        }
-        function handleOtherResponse(response, layer, loc) {
-            return response.text().then(text => {
-                var c = document.createElement('iframe');
-                c.csp = "script-src 'none'";
-                c.style = "border: none";
-                c.srcdoc = text;
-                layer.bindPopup(c, popupOptions).openPopup(loc);
-            });
-        }
-    },
-    onAdd: function (map) {
-      for (var i=0;i<this._templates.length;i++) {
-        if (this._templates[i].type !== 'query') {
-          map.addLayer(this._templates[i].layer);
-        }
-      }
-    },
+    }
+  },
 //  setZIndex: function (zIndex) {
 //      this.options.zIndex = zIndex;
 //      this._updateZIndex();
